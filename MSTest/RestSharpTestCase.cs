5e6efbf32f43111dfff54c6ffc653aada2ad3334
Chetan2028 <bmchetan2028@gmail.com>
2020-12-01 23:41:08 +0530

    [Chetan] Add : Added test case to add multiple employees

--- MSTest/RestSharpTestCase.cs
+++ MSTest/RestSharpTestCase.cs
@@ -1,6 +1,7 @@
 using AddressBook_RestAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -53,6 +54,43 @@ namespace MSTest
                     " Address : " + address.Address + " City : " + address.City + " State : " + address.State + "  Zip :" + address.Zip
                     +" PhoneNumber : " + address.PhoneNumber + "  Type : " + address.Type);
             }
+        }
+
+        [TestMethod]
+        public void GivenContact_OnPost_ShouldReturnMultipleContacts()
+        {
+            //Adding multiple contacts to list
+            List<AddressBook> multipleContactList = new List<AddressBook>();
+            multipleContactList.Add(new AddressBook { FirstName = "Sandy", LastName = "Pujeri", Zip = "590016", PhoneNumber = "89514236445", Address = "Hampi Nagar", City = "Bangalore", State = "Karnataka", Type = "Friend" });
+            multipleContactList.Add(new AddressBook { FirstName = "Ash", LastName = "Pise", Zip = "590486", PhoneNumber = "7412569841", Address = " WhiteField", City = "Bangalore", State = "Karnataka", Type = "Friend" });
+            multipleContactList.Add(new AddressBook { FirstName = "Daya", LastName = "Karagi", Zip = "590176", PhoneNumber = "89519876445", Address = "Maple Beach", City = "Karwar", State = "Karnataka", Type = "Family" });
+
+            multipleContactList.ForEach(contactData =>
+            {
+                //Arrange
+                RestRequest request = new RestRequest("/addressBook", Method.POST);
+                JObject jObjectBody = new JObject();
+                jObjectBody.Add("FirstName", contactData.FirstName);
+                jObjectBody.Add("LastName", contactData.LastName);
+                jObjectBody.Add("Zip", contactData.Zip);
+                jObjectBody.Add("Address", contactData.Address);
+                jObjectBody.Add("City", contactData.City);
+                jObjectBody.Add("State", contactData.State);
+                jObjectBody.Add("PhoneNumber", contactData.PhoneNumber);
+                jObjectBody.Add("Type", contactData.Type);
+
+                request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+
+                //Act
+                IRestResponse response = client.Execute(request);
+
+                //Assert
+                Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                AddressBook dataResponse = JsonConvert.DeserializeObject<AddressBook>(response.Content);
+                Assert.AreEqual(contactData.FirstName, dataResponse.FirstName);
+                Assert.AreEqual(contactData.LastName, dataResponse.LastName);
+                Console.WriteLine(response.Content);
+            });
         }
     }
 }
